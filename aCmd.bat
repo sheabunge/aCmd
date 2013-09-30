20eb9abe8ca3c1380932281c1e267988570778d1
Shea Bunge <info@bungeshea.com>
2013-10-01 09:38:54 +1000

    Load commands at startup from a user.bat file in the current directory if it exists

--- aCmd.bat
+++ aCmd.bat
@@ -3,6 +3,7 @@ title Another Command Prompt
 color 0F
 echo Another Command Prompt
 echo Copyright (c) 2011 Shea Bunge (http://bungeshea.com)
+if exist user.bat call user.bat
 :start
 echo.
 echo CD: %cd%
