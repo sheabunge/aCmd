afe68784da24ca755fcef6d5578a315b1283720a
Shea Bunge <info@bungeshea.com>
2013-10-01 09:42:43 +1000

    Also call user.bat from the %APPDATA% directory

--- aCmd.bat
+++ aCmd.bat
@@ -4,6 +4,7 @@ color 0F
 echo Another Command Prompt
 echo Copyright (c) 2011 Shea Bunge (http://bungeshea.com)
 if exist user.bat call user.bat
+if exist "%APPDATA%\user.bat" call "%APPDATA%\user.bat"
 :start
 echo.
 @echo on
